34a89fcc69e9371430ae331cd18c3cfe3453d8b0
SUESTRALROPIET <syeonc1008@gmail.com>
2021-11-15 18:14:25 +0900

    1114 APS"

--- APS/SWEA/5987_달리기/s2.py
+++ APS/SWEA/5987_달리기/s2.py
@@ -1,42 +1,7 @@
-## 5987_달리기
-https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWaJ4g86WA4DFAUQ&categoryId=AWaJ4g86WA4DFAUQ&categoryType=CODE&problemTitle=5987&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+import sys
+sys.stdin = open('input.txt')
 
-#### 1. 순열로 푸니 당연히 큰 수에서 시간초과
-```python
-def get_order(k):
-    if k == N:
-        check_valid(player_list)
-    
-    for h in range(k, N):
-        player_list[k], player_list[h] = player_list[h], player_list[k]
-        get_order(k+1)
-        player_list[k], player_list[h] = player_list[h], player_list[k]
-
-def check_valid(per_list):
-    global cnt
-    for condition in condition_list:
-        a, b = condition
-        if not per_list[a-1] < per_list[b-1]:
-            return
-    cnt += 1
-
-T = int(input())
-for test in range(1, T+1):
-    N, M = map(int, input().split())
-
-    player_list = [ i for i in range(1, N+1) ]
-    condition_list = [list(map(int, input().split())) for _ in range(M)]
-    cnt = 0
-
-    get_order(0)
-
-    print('#{} {}'.format(test, cnt))
-```
-
-#### 2. 비트마스킹 & 메모이제이션 & DP로 풀기
-
-```bash
-# 비트연산으로 표현한 경우의 수     		 # 각 원소 표현하기
+# 비트연산으로 표현한 경우의 수      # 각 원소 표현하기
 #   [ 3, 2, 1 ]                         #  [ 3, 2, 1 ]
 #   [ 2, 1, 0 ]
 # 0 : 0  0  0
@@ -47,9 +12,7 @@ for test in range(1, T+1):
 # 5 : 1  0  1
 # 6 : 1  1  0
 # 7 : 1  1  1
-```
 
-```python
 def dp(now_status):
     # 이미 계산된 경우의 수라면,
     # 더 이상 계산하지 않고 저장된 값 반환하기
@@ -92,4 +55,5 @@ for test in range(1, T+1):
     result = dp(0)  # 아직 아무도 안 들어온 상태에서 경우의 수 체크하기
 
     print('#{} {}'.format(test, result))
-```
+
+
