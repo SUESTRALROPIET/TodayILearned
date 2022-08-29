90e07b7f2c822871f02493aca3b0a85dbfb51096
sue <SyeonC1008@gmail.com>
2022-08-29 17:29:44 +0900

    0829 aps

---
+++ APS/programmers/Lv2_숫자의표현/s1.py
@@ -0,0 +1,35 @@
+def solution(n):
+    answer = 1
+    '''
+    연속된 숫자의 합인 아래 규칙 활용
+        1: n
+        2: 2n + 1
+        3: 3n
+        4: 4n + 2
+        5: 5n
+        6: 6n + 3
+        ...
+    '''
+    for i in range(2, n//2 + 2):
+        share, remain = divmod(n, i)
+
+        # 유효성 검사: 17번 테케 해결
+        if i % 2 == 0:
+            min_num = share - (i // 2 - 1)
+        else:
+            min_num = share - (i // 2) 
+            
+        if min_num <= 0:
+            continue
+
+        # i가 짝수/홀수 일때 조건
+        if i % 2 == 0 and remain != i // 2:
+            continue
+            
+        elif i % 2 and remain:
+            continue
+
+        answer += 1
+    
+    return answer
+
---
+++ APS/programmers/Lv2_숫자의표현/s2.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = 0
+    for i in range(1, num + 1):
+        s = 0
+        while s < num:
+            s += i
+            i += 1
+        if s == num:
+            answer += 1
+
+    return answer
